1281db316330b688c3e02b5bec11174095015906
MogesBekele <mogesbekele32@gmail.com>
2025-04-22 16:58:54 +0300

    array method in typescript

--- typescript/src/App.tsx
+++ typescript/src/App.tsx
@@ -4,6 +4,13 @@ let name: string;
 
 name = 'sjs'
 
+let age: number;
+age =38
+let isStudent: boolean;
+let hobbies: string[];
+let role: [number, string];
+
+
 const App = () => {
   return (
     <div>App</div>
