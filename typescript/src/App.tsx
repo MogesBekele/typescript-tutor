2c711626232efcc8f435ab13af3efe5b1f65718b
MogesBekele <mogesbekele32@gmail.com>
2025-04-23 17:41:26 +0300

    commit on to list

--- typescript/src/App.tsx
+++ typescript/src/App.tsx
@@ -11,12 +11,26 @@ const App: React.FC = () => {
 
   console.log(todo);
   
-  return <div>
+  return 
+  <div>
 
     <InputField todo= {todo} setTodo={setTodo} />
+    <div className="flex flex-col items-center justify-center min-h-screen bg-gray-100">
+      <h1 className="text-3xl font-bold mb-4">Todo List</h1>
+      <div className="bg-white p-4 rounded-lg shadow-md w-full max-w-md">
+        <ul className="list-disc pl-5">
+          {todos.map((todo) => (
+            <li key={todo.id} className="mb-2">
+              {todo.todo}
+            </li>
+          ))}
+        </ul>
+      </div>
+      <div/>
     
     
-  </div>;
+    
+ 
 };
 
 export default App;
