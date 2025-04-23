9ca7b7d8f9fe76b85a1e560fd6e385c8cc398515
MogesBekele <mogesbekele32@gmail.com>
2025-04-23 17:42:18 +0300

    todo app compolition

--- typescript/src/App.tsx
+++ typescript/src/App.tsx
@@ -2,36 +2,29 @@ import React, { useState } from "react";
 import InputField from "./component/InputField";
 import { Todo } from "./model";
 
-
-
-
 const App: React.FC = () => {
   const [todo, setTodo] = useState<string>("");
   const [todos, setTodos] = useState<Todo[]>([]);
 
   console.log(todo);
-  
-  return 
-  <div>
 
-    <InputField todo= {todo} setTodo={setTodo} />
-    <div className="flex flex-col items-center justify-center min-h-screen bg-gray-100">
-      <h1 className="text-3xl font-bold mb-4">Todo List</h1>
-      <div className="bg-white p-4 rounded-lg shadow-md w-full max-w-md">
-        <ul className="list-disc pl-5">
-          {todos.map((todo) => (
-            <li key={todo.id} className="mb-2">
-              {todo.todo}
-            </li>
-          ))}
-        </ul>
+  return (
+    <div>
+      <InputField todo={todo} setTodo={setTodo} />
+      <div className="flex flex-col items-center justify-center min-h-screen bg-gray-100">
+        <h1 className="text-3xl font-bold mb-4">Todo List</h1>
+        <div className="bg-white p-4 rounded-lg shadow-md w-full max-w-md">
+          <ul className="list-disc pl-5">
+            {todos.map((todo) => (
+              <li key={todo.id} className="mb-2">
+                {todo.todo}
+              </li>
+            ))}
+          </ul>
+        </div>
       </div>
-      <div/>
-    
-    
-    
- 
+    </div>
+  );
 };
 
 export default App;
-
