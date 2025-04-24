149af9af7023abd1415270a88c930e41cfc704d8
MogesBekele <mogesbekele32@gmail.com>
2025-04-24 16:59:22 +0300

    handleadd

--- typescript/src/App.tsx
+++ typescript/src/App.tsx
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import InputField from "./component/InputField";
-import { Todo } from "./model";
+import { Todo } from "./Model";
 
 const App: React.FC = () => {
   const [todo, setTodo] = useState<string>("");
@@ -8,9 +8,29 @@ const App: React.FC = () => {
 
   console.log(todo);
 
+  const handleAdd = (e: React.FormEvent) => {
+    e.preventDefault();
+    if (todo) {
+      setTodos([...todos, { id: Date.now(), todo, isDone: false }]);
+      setTodo("");
+    }
+  };
+  const handleDelete = (id: number) => {
+    setTodos(todos.filter((todo) => todo.id !== id));
+  };
+  const handleDone = (id: number) => {
+    setTodos(
+      todos.map((todo) =>
+        todo.id === id ? { ...todo, isDone: !todo.isDone } : todo
+      )
+    );
+  };
+
   return (
     <div>
       <InputField todo={todo} setTodo={setTodo} />
+
+
     </div>
   );
 };
