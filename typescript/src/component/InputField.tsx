eb98440178e489de1e81fb9defd7d529821a925a
MogesBekele <mogesbekele32@gmail.com>
2025-04-24 17:29:24 +0300

    commit on useref

--- typescript/src/component/InputField.tsx
+++ typescript/src/component/InputField.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useRef } from "react";
 
 interface Props {
   todo: string;
@@ -7,11 +7,19 @@ interface Props {
 
 }
 const InputField = ({ todo, setTodo, handleAdd}:Props) => {
+  const inputRef= useRef(null)
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setTodo(e.target.value); // Update the todo state with the input value
+  };
   return (
     <form onSubmit={handleAdd} className="flex justify-between items-center bg-white p-4 rounded-2xl shadow-md w-full max-w-md mx-auto mt-6">
       <input
+        ref={inputRef}
+
+     
+        
       value={todo}
-      onChange={(e)=>setTodo(e.target.value)}
+    
         type="text"
         placeholder="Enter a task"
         className="flex-1 border border-gray-300 rounded-lg p-2 mr-3 focus:outline-none focus:ring-2 focus:ring-blue-500"
