3f3b29ad58fd00688d3e7259f50011b252008e5f
MogesBekele <mogesbekele32@gmail.com>
2025-04-26 16:26:20 +0300

    create singletodo component

---
+++ typescript/src/component/SingleTodo.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+
+const SingleTodo = props => {
+  return (
+    <div>SingleTodo</div>
+  )
+}
+
+SingleTodo.propTypes = {}
+
+export default SingleTodo
